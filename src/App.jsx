0bd6c9196134535191ea2994bc64d2e36de71f84
nick chen <qche.au@gmail.com>
2025-04-24 10:07:24 +1000

    init: set up React project with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Docker + React</h1>
+      <p className='read-the-docs'>Hello From Docker React Project</p>
+    </>
+  );
+}
+
+export default App;
